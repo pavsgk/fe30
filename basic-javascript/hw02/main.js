bd944b236efeee521510cc5d1fc65b8549a89104
Pavel S <[omitted]>
2021-06-30 12:28:19 +0300

    [+] hw02

---
+++ basic-javascript/hw02/main.js
@@ -0,0 +1,45 @@
+function askNum() {
+  let num;
+  while (!Number.isInteger(+num) || isNaN(+num) || num === null || num === '') {
+    num = prompt('Enter number');
+  }
+  return +num;
+}
+
+function findMultiples () {
+  const num = askNum();
+  const result = [];
+  for (let i = 0; i <= num; i++) {
+    if (i % 5 === 0)
+    result.push(i);
+  }
+  if (result.length < 1) {
+    console.log('Sorry, no numbers');
+  } else {
+    console.log(...result);
+  }
+  return result;
+}
+
+function findPrimes () {
+  const m = askNum();
+  const n = askNum();
+  const result = Array(n).fill(false);
+  for (let i = m; i <= n; i++) {
+    if ((i % 2 === 0 && i !== 2) || i < 2) continue;
+    let div = 0;
+    for (let j = 2; j <= i; j++) {
+      if (i === j) {
+        break;
+      }
+      if (i % j === 0) {
+        div++;
+        break;
+      }
+    }
+    if (div === 0) result[i] = true;
+  }
+  console.log(...result.map( (x, index) => {
+    if (result[index] === true) return index;
+  }).filter( x => x ));
+}
