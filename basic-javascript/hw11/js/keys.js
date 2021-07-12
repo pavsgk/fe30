4d19d2b685e0199d15a5b9d6f6e6c1d00f89e2e0
Pavel S <pavmerk@gmail.com>
2021-07-12 22:36:28 +0300

    [*] multilayout fix

--- basic-javascript/hw11/js/keys.js
+++ basic-javascript/hw11/js/keys.js
@@ -6,8 +6,14 @@ class KeyLogger {
     this.elms.forEach(e => e.onkeydown = k => k.keyCode === 13 ? k.preventDefault() : false);
   }
   set key(x) {
-    if(x.length > 5 || x.length === 0) return;
-    this._key = x.toLowerCase();
+    if (x.length > 0 && x.length < 15 && !x.includes('Key')) {
+      this._key = x;
+    } else if (x.includes('Key')) {
+      this._key = x[3];
+    } else {
+      return;
+    }
+    this._key = this._key.toLowerCase();
     this.elms.forEach( e => {
       if (e.innerText.toLowerCase() === this._key) {
         e.classList.add(this.activeClass);
