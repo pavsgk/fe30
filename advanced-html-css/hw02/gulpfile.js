e8c4955fe3844b87b93dcacd70d613b8d7d82616
Pavel S <pavmerk@gmail.com>
2021-09-08 00:55:17 +0300

    [+] add 768

--- advanced-html-css/hw02/gulpfile.js
+++ advanced-html-css/hw02/gulpfile.js
@@ -2,6 +2,8 @@ import gulp from 'gulp';
 import clean from 'gulp-clean';
 import autoprefixer from 'gulp-autoprefixer';
 import cleanCSS from 'gulp-clean-css';
+import concat from 'gulp-concat';
+import minify from 'gulp-minify';
 
 import BS from 'browser-sync';
 const browserSync = BS.create();
@@ -13,17 +15,18 @@ const sass = gulpSass(dartSass);
 gulp.task('clean', () => gulp.src(['dist/css/*', 'dist/js/*'], {read: false}).pipe(clean()));
 gulp.task('buildCss', () => gulp.src('src/scss/**/*', {allowEmpty: true})
   .pipe(sass())
-  // .pipe(autoprefixer({cascade: false}))
-  // .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(autoprefixer({cascade: false}))
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(concat('styles.min.css'))
   .pipe(gulp.dest('dist/css')));
 
 gulp.task('buildJs', () => gulp.src('src/js/**/*', {allowEmpty: true})
+  .pipe(concat('scripts.js'))  
+  .pipe(minify({ext:{min: '.min.js'}}))
   .pipe(gulp.dest('dist/js')));
 
 gulp.task('buildImg', () => gulp.src('src/img/**/*', {allowEmpty: true})
   .pipe(gulp.dest('dist/img')));
-
-
 
 gulp.task('build', gulp.series('clean', 'buildCss', 'buildJs', 'buildImg'));
 
--- advanced-html-css/hw02/src/js/main.js
+++ advanced-html-css/hw02/src/js/main.js
@@ -1,2 +1,10 @@
-const burgerButton = document.querySelector('.head__nav-btn');
-burgerButton.addEventListener('click', () => document.querySelector('.head__menu').classList.toggle('head__menu--visible'));
+const navButton = document.querySelector('.head__nav-btn');
+const navMenu = document.querySelector('.head__menu');
+
+navButton.addEventListener('click', () => document.querySelector('.head__menu').classList.toggle('head__menu--mobile'));
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    navMenu.classList.remove('head__menu--mobile');
+  }
+});
