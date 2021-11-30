edfc52c300027604dce213fef4cbbc133b63c886
Pavlo Sapatsynskyi <pavmerk@gmail.com>
2021-11-30 14:56:43 +0200

    [+] add cart state

--- react/rhw02/src/App.js
+++ react/rhw02/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.scss';
 import ShopPage from './pages/ShopPage';
 import randomHexColor from './utils/randomHexColor';
@@ -7,35 +7,40 @@ const shopUrl = 'https://script.google.com/macros/s/AKfycbzEYFV5JBtD5d2D2swAs6i9
 const shopUrlName = '___cakeShop';
 const backgroundColors = Array.from({ length: 100 }, () => randomHexColor(0.2));
 
-function App() {
+class App extends React.Component {
 
-  const [goods, setGoods] = useState([]);
+  state = {
+    goods: [],
+    cart: []
+  }
 
-  useEffect(() => (async () => {
+  componentDidMount() {
+    (async () => {
     
-    const responce = await fetch(`${shopUrl}${shopUrlName}`)
-      .then(res => res.text())
-      .then(text => JSON.parse(text));
-
-    const rawFavorites = localStorage.getItem('favorites');
-    const favorites = rawFavorites !== null ? JSON.parse(rawFavorites) : [];
-
-    if (responce.length > 0) {
-      responce.forEach(e => {
-        e.backgroundColor = backgroundColors[Number(e.id)];
-        if (favorites[e.id] === true) {
-          e.isFav = true;
-          return;
-        }
-        e.isFav = false;
-      })
-    };
-
-    setGoods(responce);
-  })(), []);
+      const responce = await fetch(`${shopUrl}${shopUrlName}`)
+        .then(res => res.text())
+        .then(text => JSON.parse(text));
   
-  const toggleFav = (id) => {
-    const newState = [...goods];
+      const rawFavorites = localStorage.getItem('favorites');
+      const favorites = rawFavorites !== null ? JSON.parse(rawFavorites) : [];
+  
+      if (responce.length > 0) {
+        responce.forEach(e => {
+          e.backgroundColor = backgroundColors[Number(e.id)];
+          if (favorites[e.id] === true) {
+            e.isFav = true;
+            return;
+          }
+          e.isFav = false;
+        })
+      };
+  
+      this.setGoods(responce);
+    })()
+  }
+  
+  toggleFav(id) {
+    const newState = [...this.state.goods];
     const localStorageNewState = {};
     newState.forEach(e => {
       if (e.id === id) {
@@ -45,15 +50,37 @@ function App() {
     });
 
     localStorage.setItem('favorites', JSON.stringify(localStorageNewState));
-    setGoods(newState);
+    this.setGoods(newState);
+  }
+
+  addCart(id, count = 1) {
+    const newState = [...this.cart];
+    const inCartIndex = this.cart.findIndex(({ id: currentId })=> id === currentId);
+    if (inCartIndex > -1) {
+      newState[inCartIndex].count += 1 * count;
+    } else {
+      newState.push({
+        id,
+        count: 1 * count,
+      });
+    }
   }
 
 
-  return (
-    <>
-      <ShopPage goods={goods} toggleFav={toggleFav} />
-    </>
-  );
+
+  setGoods(newGoods = []) {
+    this.setState({
+      goods: newGoods,
+    })
+  }
+
+
+  render() {
+    const { goods } = this.state;
+    return (
+      <ShopPage goods={goods} toggleFav={(id) => this.toggleFav(id)} />
+    );
+  } 
 }
 
 export default App;
